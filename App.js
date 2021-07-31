1409ff23dbb959ac698cc5d862623fc72166baa4
Jahid Hasan <slniloy500@gmail.com>
2021-07-31 10:02:15 +0600

    creacte screen

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet,Text} from 'react-native';
+import Home from './screen/Home';
+
+const App = () => {
+  return (
+    <Home/>
+    
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default App;
---
+++ screen/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>This is home screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Home;
